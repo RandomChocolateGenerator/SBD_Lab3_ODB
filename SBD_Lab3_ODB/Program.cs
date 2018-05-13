71f2c71bf5a06c052446557be2404ac8afbafcb7
RandomChocolateGenerator <dorota.burka@student.wat.edu.pl>
2018-05-13 10:58:19 +0200

    Wstępne działania z bazą na obiektach.

--- SBD_Lab3_ODB/Program.cs
+++ SBD_Lab3_ODB/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SBD_Lab3_ODB.Database.Controllers;
 using SBD_Lab3_ODB.Database.Objects;
 using Sqo;
 
@@ -10,10 +11,19 @@ namespace SBD_Lab3_ODB
 {
     class Program
     {
+        static DBController dbcontroller;
+
         static void Main(string[] args)
         {
             var dog = new Dog("male", "Roki", "Coton de tulear");
-            var doge = new Doge();
+            //var doge = new Doge();
+            var cat = new Cat("female", "Dafne", "British Shorthair");
+
+            dbcontroller = new DBController("/DBFiles");
+            dbcontroller.Add(dog);
+            //dbcontroller.Add(cat);
+
+            dbcontroller.printDB();
 
             Console.ReadKey();
         }
