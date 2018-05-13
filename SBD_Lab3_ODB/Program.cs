0b9a095384cf8e85eea0563c4d585c80d683c048
userndl <39233910+userndl@users.noreply.github.com>
2018-05-13 04:51:00 -0700

    Dodane SiaqodbFactory, tworzenie pliku bazy

--- SBD_Lab3_ODB/Program.cs
+++ SBD_Lab3_ODB/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SBD_Lab3_ODB.Database.Controllers;
+using SBD_Lab3_ODB.Database;
 using SBD_Lab3_ODB.Database.Objects;
 using Sqo;
 
@@ -15,17 +16,25 @@ namespace SBD_Lab3_ODB
 
         static void Main(string[] args)
         {
-            var dog = new Dog("male", "Roki", "Coton de tulear");
-            //var doge = new Doge();
-            var cat = new Cat("female", "Dafne", "British Shorthair");
+            Dog dog = new Dog("male", "Roki", "Coton de tulear");
+            Cat cat = new Cat("female", "Dafne", "British Shorthair");
 
-            dbcontroller = new DBController("/DBFiles");
-            dbcontroller.Add(dog);
+            Siaqodb siaqodb = SiaqodbFactory.GetInstance();
+
+            siaqodb.StoreObject(dog);
+
+
+            SiaqodbFactory.CloseDatabase();
+
+            //Siaqodb db = new Siaqodb();
+            //db.
+            //dbcontroller = new DBController("/DBFiles");
+            //dbcontroller.Add(dog);
             //dbcontroller.Add(cat);
 
-            dbcontroller.printDB();
+            //dbcontroller.printDB();
 
-            Console.ReadKey();
+            //Console.ReadKey();
         }
     }
 }
